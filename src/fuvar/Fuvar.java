6fb2c0701e1a8b9e55d33fb75357324b0a613d8e
VBalazs <v.vbalazs@gmail.com>
2021-02-12 12:14:59 +0100

    most már be tudom olvasni a fájt

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,27 +1,58 @@
 
 package fuvar;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Fuvar {
+    private int taxi_id;
+    private String indulas;
+    private double tavolsag;
+    private double viteldij;
+    private double borravalo;
+    private String fizetes_modja;
 
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("fuvar.csv");
-        List<String> sorok = Files.readAllLines(path);
-        List<FuvarBont> fuvarok = new ArrayList<>();
-        
-        for (int i = 1; i < sorok.size(); i++) {
-            fuvarok.add(new FuvarBont(sorok.get(i)));
-        }
-        //System.out.println(fuvarok.size());
-        
-        
+    
+ //   
+
+    public Fuvar(String sor) {
+        String []s = sor.split(";");
+        this.taxi_id = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.tavolsag = Double.parseDouble(s[2]);
+        this.viteldij = Double.parseDouble(s[3]);
+        this.borravalo = Double.parseDouble(s[4]);
+        this.fizetes_modja = s[5];
+    }
+ 
+    
+
+    public int getTaxi_id() {
+        return taxi_id;
+    }
+
+    public String getIndulas() {
+        return indulas;
+    }
+
+    public double getTavolsag() {
+        return tavolsag;
+    }
+
+    public double getViteldij() {
+        return viteldij;
+    }
+
+    public double getBorravalo() {
+        return borravalo;
+    }
+
+    public String getFizetes_modja() {
+        return fizetes_modja;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "FuvarBont{" + "taxi_id=" + taxi_id + ", indulas=" + indulas + ", tavolsag=" + tavolsag + ", viteldij=" + viteldij + ", borravalo=" + borravalo + ", fizetes_modja=" + fizetes_modja + '}';
     }
     
 }
---
+++ src/fuvar/FuvarProgram.java
@@ -0,0 +1,27 @@
+
+package fuvar;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FuvarProgram {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("fuvar.csv");
+        List<String> sorok = Files.readAllLines(path);
+        List<Fuvar> fuvarok = new ArrayList<>();
+        
+        for (int i = 1; i < sorok.size(); i++) {
+            fuvarok.add(new Fuvar(sorok.get(i)));
+        }
+        System.out.println(fuvarok.size());
+        
+        
+    }
+    
+}
