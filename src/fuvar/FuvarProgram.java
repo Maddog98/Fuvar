378d1b72eb9cc6d78541fe0eed1a034456766c82
VBalazs <v.vbalazs@gmail.com>
2021-02-16 12:28:17 +0100

    Tovább lépve

--- src/fuvar/FuvarProgram.java
+++ src/fuvar/FuvarProgram.java
@@ -1,4 +1,3 @@
-
 package fuvar;
 
 import java.io.IOException;
@@ -8,30 +7,40 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class FuvarProgram {
 
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("fuvar.csv");
         List<String> sorok = Files.readAllLines(path);
         List<Fuvar> fuvarok = new ArrayList<>();
-        
+
         for (int i = 1; i < sorok.size(); i++) {
             fuvarok.add(new Fuvar(sorok.get(i)));
         }
-        System.out.println("3. feladat: "+fuvarok.size()+" fuvar");
-        
-        int maxfuvar=0;
-        for (int i = 1; i < sorok.size(); i++) {
-            if (sorok.get(i)>maxfuvar) {
-                maxfuvar=sorok.get(i);
+        System.out.println("3. feladat: " + fuvarok.size() + " fuvar");
+
+        double bevetel = 0.0;
+
+        for (int i = 1; i < fuvarok.size(); i++) {
+            if (fuvarok.get(i).getTaxi_id() == 6185) {
+                bevetel += fuvarok.get(i).getBorravalo();
             }
         }
-        
-        
-        System.out.println("4. feladat:");
-        
-        
+
+        System.out.println("4. feladat: 4 fuvar alatt: " + bevetel + " $");
+
+        System.out.println("5. feladat");
+
+        double osszzMerfold = 0;
+        System.out.println("6. feladat:");
+        for (int i = 1; i < fuvarok.size(); i++) {
+            osszzMerfold = osszzMerfold + fuvarok.get(i).getTavolsag();
+        }
+        double vegered = osszzMerfold / 1.6;
+        System.out.println(osszzMerfold);
+//        System.out.println("6. feladat : "+vegered+" km");
+        System.out.println(vegered);
+
     }
-    
+
 }
